35ffec3b708c1fb407e8f169e7d007b28bbf92e0
Antonio Servin <antonio.servin.m11@gmail.com>
2021-06-10 20:57:45 -0500

    Add first exercise

---
+++ ejercicios_sesion4.py
@@ -0,0 +1,6 @@
+# Ejercicio 1
+suma = lambda x,y : x + y if isinstance(x,int) & isinstance(y,int) else str(x)+ str(y)
+suma(10,10)
+
+#  Ejercicio 2
+# leerdiccionario = lambda diccionario : f'El diccionario tiene como la llave {diccionario.}'
